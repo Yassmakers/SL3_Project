e05dc1b545c325adf8ebde88bdf75bf8b62597fb
Yassine Messa <89946578+Yassmakers@users.noreply.github.com>
2023-10-31 14:32:10 +0100

    Fixed a issue TO DO: UI, CRUD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.sl3verbeterd"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.sl3verbeterd"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

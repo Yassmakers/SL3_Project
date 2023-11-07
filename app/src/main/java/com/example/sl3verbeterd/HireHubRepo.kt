65a836279d6b91b0fb35e5b59b0cb63a73863060
Yassine Messa <89946578+Yassmakers@users.noreply.github.com>
2023-11-07 19:19:20 +0100

    Revert "Delete button voor crud fix"
    This reverts commit d6fc0de3aca1525b7f4be80c3cf0f97e7b84d790.

--- app/src/main/java/com/example/sl3verbeterd/HireHubRepo.kt
+++ app/src/main/java/com/example/sl3verbeterd/HireHubRepo.kt
@@ -2,14 +2,9 @@ package com.example.sl3verbeterd
 
 import androidx.annotation.WorkerThread
 import kotlinx.coroutines.flow.Flow
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 
-class HireHubRepository(private val hireHubDao: HireHubDao, private val context: Context) {
+class HireHubRepository(private val hireHubDao: HireHubDao) {
+
     // Room executes all queries on a separate thread.
     // Observed Flow will notify the observer when the data has changed.
     val allProfiles: Flow<List<Profile>> = hireHubDao.getProfilesOrderedByFirstName()
